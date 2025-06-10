57222d98bfbc74ce44790c55a5bc323a2f1a0cd1
j harishankar <jharivichu27@gmail.com>
2025-06-10 21:04:36 +0530

    car's and turtle created

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -3,12 +3,16 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+adam = Player()
+car_manager = CarManager()
 game_is_on = True
+screen.listen(),
+screen.onkeypress(adam.up,"Up")
 while game_is_on:
     time.sleep(0.1)
+    car_manager.create_cars()
+    car_manager.move_cars()
     screen.update()
