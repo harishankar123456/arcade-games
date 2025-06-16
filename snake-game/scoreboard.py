3ec7125fdb40800fe6a3f242bf876f60b393d9a6
j harishankar <jharivichu27@gmail.com>
2025-06-16 23:15:11 +0530

    snake game now maintains permanent high score

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -4,7 +4,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -18,6 +19,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score>self.highscore:
             self.highscore = self.score
+        with open("data.txt",mode='w') as data:
+            data.write(f"{self.highscore}")
         self.score = 0
         self.clear()
         self.write(f"Score:{self.score} High Score:{self.highscore}", align="center", font=('Arial', 24, "normal"))
